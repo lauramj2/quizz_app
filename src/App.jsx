9826bb57f1102cebc37a7f19fae06f285f5d6190
lauramj2 <lauraamarie2@gmail.com>
2026-02-07 13:28:57 -0500

    game is functional
    questions render, can select answers and press button to see if answers are correct, can start new game

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect, useRef } from "react"
 import { nanoid } from "nanoid"
+import he from "he"
 
 import Questions from "./Components/Questions.jsx"
+
 
 export default function App() {
 
   const [questions, setQuestions] = useState([])
+  const [results, setResults] = useState({})
+  const [checked, setChecked] = useState(false)
+  const [scoreMsg, setScoreMsg] = useState("")
+
   const fetchedRef = useRef(false) // track if fetch already happened
 
   useEffect(() => {
@@ -24,8 +30,15 @@ export default function App() {
         const data = await res.json()
 
         if (Array.isArray(data.results)){
-          console.log(data.results)
-          setQuestions(data.results)
+          const questionsWithId = data.results.map(question => ({
+            ...question,
+            id: nanoid(),
+            question: he.decode(question.question),
+            correct_answer: he.decode(question.correct_answer),
+            incorrect_answers: question.incorrect_answers.map(ans => he.decode(ans))
+          }))
+          console.log(questionsWithId)
+          setQuestions(questionsWithId)
         } else {
           setQuestions([])
         }
@@ -36,31 +49,88 @@ export default function App() {
     }
 
     getQuestions()
-
   }, [])
 
-  const questionElements = questions.map((question) => {
+
+  function handleSelect(questionId, isCorrect){
+    setResults(prev => ({
+      ...prev,
+      [questionId]: isCorrect
+
+      //ex: questionId: "q3", isCorrect: true becomes ===> q3: true
+      //square brackets = use value of the variable as the key
+      //marks every questions either true or false
+      //updates every time something is selected, updates if answer was changed etc
+    }))
+  }
+
+  function checkAnswers(){
+    if (Object.values(results).length !== questions.length){
+      alert("Please answer all questions")
+      return
+    }
+
+    setChecked(true)
+    //mark that answers were checked
+  
+    const values = Object.values(results)
+    //Object.values(results) turns object into array of values
+    const correctAnswers = values.filter(value => value === true)
+    //filter keeps only true values
+    const score = correctAnswers.length
+
+    setScoreMsg(`You scored ${score} / ${questions.length}`)
+ 
+  }
+
+  function newGame(){
+    setChecked(false)
+    setResults({})
+    setScoreMsg("")
+    setQuestions(prev => prev.map(question => ({
+                                ...question,
+                                id: nanoid()
+                              }))
+                )
+  }
+
+
+
+  const questionElements = questions.map(question => {
     return (
         <Questions
-          key={question.question}
+          key={question.id}
+          questionId={question.id}
           question={question.question}
           correctAnswer={question.correct_answer}
           incorrectAnswer={question.incorrect_answers}
+          onSelectAnswer={handleSelect}
+          checked={checked}
+          selectedResult={results[question.id]}
         />
     )
   })
 
+
   return (
-    <>
-      <h1>Test</h1>
+    <main>
       {(!Array.isArray(questions) || questions.length === 0) ? 
         (
-          <p>Loading Questions</p>
+          <p>Loading Questions...</p>
         ):
         (
           questionElements
         )
       }
-    </>
+      {checked === false && <div className="checkDiv">
+        <button className="checkBtn" onClick={checkAnswers}>Check Answers</button>
+      </div>}
+
+      {checked === true && <div className="newGameDiv">
+        <p>{scoreMsg}</p>
+        <button className="newBtn" onClick={newGame}>Play Again</button>
+      </div>}
+
+    </main>
   )
 }
--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,23 +1,64 @@
-
+import { useState } from "react"
+import { nanoid } from "nanoid"
 
 export default function Questions(props) {
     if (!props){
         return null
     }
-    const allAnswers = [...props.incorrectAnswer, props.correctAnswer]
-    const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5)
-    const correct = props.correctAnswer
+
+    const [answers, setAnswers] = useState(() => {
+        return [...props.incorrectAnswer, props.correctAnswer]
+            .sort(() => Math.random() - 0.5)
+            .map(ans => ({
+                isSelected: false,
+                id: nanoid(),
+                value: ans,
+                isCorrect: ans === props.correctAnswer
+            }))
+    })
     
+    function selectAnswer(id){
+        setAnswers(prev => {
+            return prev.map(ans => ({
+                ...ans,
+                isSelected: ans.id === id
+            }))
+        })
+
+        const selected = answers.find(ans => ans.id === id)
+        props.onSelectAnswer(props.questionId, selected.isCorrect)
+    } 
+        
+
     return (
         <div className="bank">
             <h1 className="question">{props.question}</h1>
             <ul>
-                {[...props.incorrectAnswer, props.correctAnswer]
-                .sort(() => Math.random() - 0.5)
-                .map((ans, index) => (
-                <li key={index}><button>{ans}</button></li>
-                ))}
+                {answers.map(ans => {
+                    let bColor = "white"
+                    
+                    if (props.checked) {
+                        if (ans.isSelected && !ans.isCorrect){
+                            bColor = "red"
+                        } else if (ans.isSelected && ans.isCorrect){
+                            bColor = "green"
+                        } else if (!ans.isSelected && ans.isCorrect){
+                            bColor = "green"
+                        } 
+                    } else if (ans.isSelected){
+                            bColor= "blue"
+                    }
+    
+
+                    return (
+                        <li key={ans.id}>
+                            <button style={{backgroundColor: bColor}} onClick={() => selectAnswer(ans.id)}>
+                                {ans.value}
+                            </button>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
-}
+} 
